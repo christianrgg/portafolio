df3d50516d79e7bc6abcf7fa07e2892c2300eeb0
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-02-06 02:07:41 -0600

    Link de redes y animaciones de iconos

--- dist/app.56908c73.js
+++ dist/app.56908c73.js
@@ -5592,7 +5592,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57093" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58185" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
