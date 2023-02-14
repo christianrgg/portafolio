6f9924546c3a590f1429ffbb504b9605cdbe7903
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-02-14 12:14:30 -0600

    Preguntas frecuentes y estilos CSS

--- dist/config.0874d187.js
+++ dist/config.0874d187.js
@@ -200,6 +200,10 @@ module.hot.accept(reloadCSS);
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/styles/faq.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
 },{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/js/config.js":[function(require,module,exports) {
 "use strict";
 
@@ -211,7 +215,8 @@ require("../styles/skills.scss");
 require("../styles/reviews.scss");
 require("../styles/swiper.scss");
 require("../styles/projects.scss");
-},{"../styles/base.scss":"assets/styles/base.scss","../styles/loader.scss":"assets/styles/loader.scss","../styles/landing.scss":"assets/styles/landing.scss","../styles/coollinks.scss":"assets/styles/coollinks.scss","../styles/skills.scss":"assets/styles/skills.scss","../styles/reviews.scss":"assets/styles/reviews.scss","../styles/swiper.scss":"assets/styles/swiper.scss","../styles/projects.scss":"assets/styles/projects.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+require("../styles/faq.scss");
+},{"../styles/base.scss":"assets/styles/base.scss","../styles/loader.scss":"assets/styles/loader.scss","../styles/landing.scss":"assets/styles/landing.scss","../styles/coollinks.scss":"assets/styles/coollinks.scss","../styles/skills.scss":"assets/styles/skills.scss","../styles/reviews.scss":"assets/styles/reviews.scss","../styles/swiper.scss":"assets/styles/swiper.scss","../styles/projects.scss":"assets/styles/projects.scss","../styles/faq.scss":"assets/styles/faq.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -236,7 +241,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52467" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50602" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
