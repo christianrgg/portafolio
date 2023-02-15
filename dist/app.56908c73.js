8517c50ef3f9e5634a07853b73e9def313568896
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-02-15 08:37:09 -0600

    Acordion con javascript

--- dist/app.56908c73.js
+++ dist/app.56908c73.js
@@ -15345,6 +15345,12 @@ var _data = require("./data");
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 var bar = document.querySelector(".loading__bar--inner");
 var counter_num = document.querySelector(".loading__counter--number");
 var c = 0;
@@ -15423,6 +15429,18 @@ _data.reviews.map(function (review) {
   var template = "<div class=\"swiper-slide\">\n        <div class=\"review\">\n            <svg width=\"100%\" height=\"100%\" viewBox=\"0 0 33 27\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M0.999961 20.2398C0.91996 19.0398 0.959962 17.6398 1.11996 16.0398C1.35996 14.3598 2.27997 11.9198 3.87996 8.71984C5.55996 5.43985 7.95996 2.59983 11.08 0.199829H11.68C11.52 0.359831 11.28 0.559832 10.96 0.799829C10.72 0.959831 10.24 1.39983 9.51996 2.11983C8.79996 2.83983 8.15997 3.55984 7.59996 4.27984C7.03995 4.91983 6.55995 5.71984 6.15996 6.67984C5.75997 7.55982 5.55996 8.43982 5.55996 9.31984C5.63997 10.1198 6.03996 10.9598 6.75996 11.8398C7.47996 12.6398 8.55996 13.3598 9.99996 13.9998C12.8 15.1198 14.2 17.1598 14.2 20.1198C14.2 21.7998 13.5199 23.3198 12.16 24.6798C10.88 25.9598 9.31995 26.5998 7.47996 26.5998C5.79996 26.5998 4.27995 25.9598 2.91996 24.6798C1.63995 23.3998 0.999961 21.9198 0.999961 20.2398ZM19.36 20.2398C19.28 19.0398 19.32 17.6398 19.48 16.0398C19.72 14.3598 20.64 11.9198 22.24 8.71984C23.92 5.43985 26.32 2.59983 29.44 0.199829H30.04C29.8801 0.359831 29.6401 0.559832 29.32 0.799829C29.08 0.959831 28.6 1.39983 27.88 2.11983C27.16 2.83983 26.52 3.55984 25.96 4.27984C25.48 4.91983 25 5.71984 24.52 6.67984C24.12 7.55982 23.92 8.43982 23.92 9.31984C24 10.1198 24.4 10.9598 25.12 11.8398C25.84 12.6398 26.92 13.3598 28.36 13.9998C31.1599 15.0398 32.56 17.0798 32.56 20.1198C32.56 21.7998 31.8799 23.3198 30.52 24.6798C29.2399 25.9598 27.68 26.5998 25.84 26.5998C24.16 26.5998 22.64 25.9598 21.28 24.6798C20 23.3998 19.36 21.9198 19.36 20.2398Z\"></path></svg>\n            <div class=\"review__card\">\n                <div class=\"review__topborder\"></div>\n                <div class=\"review__text\">\n                    <span>".concat(review.review.substring(0, 1), "</span>\n                    ").concat(review.review.substring(1, review.review.length), " \n                </div>\n                <img src=\"").concat(review.image, "\" alt=\"\" class=\"review__img\">\n                <div class=\"review__profile\">\n                    <span>").concat(review.name, "</span>\n                    <span>").concat(review.position, "</span>\n                </div>\n            </div>\n        </div>\n    </div>");
   swiper_container.innerHTML += template;
 });
+var questions = _toConsumableArray(document.querySelectorAll(".question"));
+questions.map(function (question) {
+  var q_text = question.querySelector("h3");
+  q_text.addEventListener("click", function () {
+    questions.filter(function (q) {
+      return q !== question;
+    }).map(function (q) {
+      return q.classList.remove("open");
+    });
+    question.classList.toggle("open");
+  });
+});
 },{"gsap":"../node_modules/gsap/index.js","swiper":"../node_modules/swiper/swiper.esm.js","./data":"assets/js/data.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -15448,7 +15466,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50602" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49826" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -100,3 +100,16 @@ reviews.map((review)=>{
     </div>`;
     swiper_container.innerHTML += template;
 });
+
+const questions = [...document.querySelectorAll(`.question`)];
+questions.map((question)=>{
+    let q_text = question.querySelector(`h3`);
+
+    q_text.addEventListener(`click`, ()=>{
+        questions
+            .filter((q) => q !== question)
+            .map((q) => q.classList.remove("open"));
+
+        question.classList.toggle(`open`);
+    });
+});
